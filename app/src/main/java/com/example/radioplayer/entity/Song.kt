4bafd74330c9d28c1a9bedd4714f4712a4ce2116
Philipp <voss.ph@web.de>
2022-12-18 14:38:13 +0100

    Update Song.kt
    Datenfeld für Image umbenannt

--- app/src/main/java/com/example/radioplayer/entity/Song.kt
+++ app/src/main/java/com/example/radioplayer/entity/Song.kt
@@ -7,11 +7,11 @@ data class Song(
     var artist: String = "",
     var year: Int = 1900,
     var album: String = "",
-    var coverImageString: String = "",
     var length: String = "00:00",
     var lengthInSeconds: Int = 0,
     var remainingDuration: Int = 0,
     var startDateTime: Date? = null,
     var endDateTime: Date? = null,
     var streamUrl: String = "",
+    var imageUrl: String = "",
 )
